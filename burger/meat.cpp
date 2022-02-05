8537f038f77101280da10d86b48e44c03dbc466f
HASANAL KARIM RAHMAN <hasanalkarimr@gmail.com>
2022-02-05 19:24:56 +0600

    auto-commit

--- burger/meat.cpp
+++ burger/meat.cpp
@@ -1,35 +1,4 @@
 i like meat
 
 
-
-fffff
-
-ooo   gygygygyg
-
-
-iuiu
-
-grftgrg
-yuyuy
-
-
-cvvdv
-
-
-         tytyt
-
-yuyu   tyty
-
-yuyu
-
-
-209209209  66666
-
-
-
-9999
-
-
-00000
-
-00000
+uvuvuvuuvuu
